9b78d797ddf352dad248c30f251966ae16a462d1
Necati Caner Gaygısız <ne.caner@hotmail.com>
2017-10-30 16:29:46 +0300

    Web scripts size reduced

--- src/actions/SetApkPathAction.java
+++ src/actions/SetApkPathAction.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (C) 2017 Necati Caner Gaygisiz
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (C) 2017 Necati Caner Gaygisiz
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package actions;
 
@@ -25,6 +25,7 @@ import com.intellij.openapi.vfs.VirtualFile;
 import managers.FileChooserDialogManager;
 import managers.PropertiesManager;
 import org.apache.http.util.TextUtils;
+import utils.FileTypes;
 import utils.PropertyKeys;
 import utils.Strings;
 
@@ -43,6 +44,7 @@ public class SetApkPathAction extends AnAction {
                             : LocalFileSystem.getInstance().findFileByPath(currentApkPath);
 
             VirtualFile apkFile = new FileChooserDialogManager.Builder(project, fileToSelectOnCreate)
+                    .setFileTypes(FileTypes.FILE)
                     .setTitle(Strings.TITLE_ASK_APK_FILE)
                     .setDescription(Strings.MESSAGE_ASK_APK_FILE)
                     .withFileFilter("apk")
